fd8ce73cd56786540ca8bc9ea8c5b7ee0308455b
JunSuHeo <wmfkf95@gmail.com>
2021-01-23 14:14:43 +0900

    백준 11399

---
+++ Q11399/Q11399.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    vector<int> arr;
+    int sum = 0;
+
+    for(int i = 0; i < N; i++){
+        int tmp;
+
+        scanf("%d", &tmp);
+        arr.push_back(tmp);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    sum += arr[0];
+
+    for(int i = 1; i < N; i++){
+        arr[i] = arr[i-1] + arr[i];
+        sum += arr[i];
+    }
+
+    printf("%d\n", sum);
+
+
+    // 12334 -> 1 3 6 9 13 -> 32
+
+    return 0;
+}
