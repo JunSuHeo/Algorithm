6427b8ea8ef3c10235b6d596c65ddc6cb5381db9
JunSuHeo <wmfkf95@gmail.com>
2021-05-08 18:18:15 +0900

    백준 10820

---
+++ Q10820/Main.java
@@ -0,0 +1,19 @@
+package Q10820;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = "";
+		ArrayList<String> arr = new ArrayList<>();
+		
+		while((input = br.readLine()) != null) {
+			arr.add(input);
+		}
+	}
+
+}
