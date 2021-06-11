5d0f716675a1f54d932b31a366de97084e3946dd
JunSuHeo <wmfkf95@gmail.com>
2021-06-11 16:28:40 +0900

    백준 11931

--- Q11931/Main.java
+++ Q11931/Main.java
@@ -1,32 +1,31 @@
-package Q10989;
+package Q11931;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
 		int N = Integer.parseInt(br.readLine());
 		
-		int [] arr = new int[10001];
+		Integer[] arr = new Integer[N];
 		
 		for(int i = 0; i < N; i++) {
-			int a = Integer.parseInt(br.readLine());
-			arr[a]++;
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 		
-		for(int i = 1; i < 10001; i++) {
-			if(arr[i] > 0) {
-				while(arr[i] != 0) {
-					bw.write(i + "\n");
-					arr[i]--;
-				}
-			}
+		Arrays.sort(arr, Collections.reverseOrder());
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < N; i++) {
+			bw.write(arr[i] + "\n");
 		}
 		
 		bw.flush();
