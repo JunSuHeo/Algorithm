8152e8543af44dcb618a4ea80c6560cc4720ddd7
JunSuHeo <wmfkf95@gmail.com>
2021-06-11 11:53:10 +0900

    백준 14425

---
+++ Q14425/Main.java
@@ -0,0 +1,34 @@
+package Q14425;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		HashSet<String> arr = new HashSet<String>();
+		
+		for(int i = 0; i < N; i++) {
+			arr.add(br.readLine());
+		}
+		
+		int cnt = 0;
+		for(int i = 0; i < M; i++) {
+			String tmp = br.readLine();
+			if(arr.contains(tmp)) {
+				cnt++;
+			}
+		}
+		
+		System.out.println(cnt);
+	}
+
+}
