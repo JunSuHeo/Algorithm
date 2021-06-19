db6064da2c38d633e0715d4ad2b0188a5e2f1f93
JunSuHeo <wmfkf95@gmail.com>
2021-06-20 03:54:40 +0900

    백준 11057

---
+++ Q11057/Main.java
@@ -0,0 +1,36 @@
+package Q11057;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[][] dp = new int[n+1][10];
+		
+		for(int i = 0; i < 10; i++) {
+			dp[1][i] = 1;
+		}
+		
+		for(int i = 2; i <= n; i++) {
+			for(int j = 0; j < 10; j++) {
+				for(int k = 0; k <= j; k++) {
+					dp[i][j] += dp[i - 1][k];
+					dp[i][j] %= 10007;
+				}
+			}
+		}
+		
+		int sum = 0;
+		for(int i = 0; i < 10; i++) {
+			sum += dp[n][i];
+			sum %= 10007;
+		}
+		
+		System.out.println(sum);
+	}
+
+}
