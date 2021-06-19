c1e68b8860cdc325b9d1e99a765d875dd2cf6fb5
JunSuHeo <wmfkf95@gmail.com>
2021-06-19 16:07:39 +0900

    백준 1966

--- Q1966/Main.java
+++ Q1966/Main.java
@@ -3,7 +3,7 @@ package Q1966;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,50 +11,57 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		
+		StringBuilder sb = new StringBuilder();
+		
 		for(int i = 0; i < n; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int printCnt = Integer.parseInt(st.nextToken());
+			Queue<Integer> q = new ArrayDeque<Integer>();
+			int m = Integer.parseInt(st.nextToken());
 			int targetIdx = Integer.parseInt(st.nextToken());
 			
-			Deque<Integer> dq = new ArrayDeque<Integer>();
-			
 			st = new StringTokenizer(br.readLine(), " ");
-			int maxNum = 0;
-
-			// input queue
-			for(int j = 0; j < printCnt; j++) {
-				int tmp = Integer.parseInt(st.nextToken());
-				dq.offer(tmp);
-				
-				if(tmp > maxNum) {
-					maxNum = tmp;
-				}
+			for(int j = 0; j < m; j++) {
+				q.add(Integer.parseInt(st.nextToken()));
 			}
 			
-			int idx = 0;
-			int count = 0;
+			int printCnt = 0;
 			
 			while(targetIdx != -1) {
-				int tmp = dq.poll();
+				int firstNum = q.peek();
+				int maxNum = 0;
 				
-				if(tmp == maxNum) {
-					if(dq.contains(tmp)) {
-						
-					} else {
-						for(int j = tmp - 1; j > 0; j--) {
-							if(dq.contains(j)) {
-								maxNum = j;
-								break;
-							}
-						}
+				for(int j = 9; j > 0; j--) {
+					if(q.contains(j)) {
+						maxNum = j;
+						break;
 					}
+				}
+				
+				if(firstNum < maxNum) {		// Do not print
+					if(targetIdx == 0) {
+						int tmp = q.poll();
+						q.add(tmp);
+						targetIdx = q.size() - 1;
+					} else {
+						int tmp = q.poll();
+						q.add(tmp);
+						targetIdx--;
+					}
+				} else {	// print
+					printCnt++;
 					
-					
+					q.poll();
+					targetIdx--;
 				}
 				
 			}
 			
+			sb.append(printCnt).append('\n');
 		}
+		
+		sb.deleteCharAt(sb.length() - 1);
+		
+		System.out.println(sb);
 	}
 }
 
