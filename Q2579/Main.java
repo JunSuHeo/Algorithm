7917a57927494ed9fbc713c72c43da96bb85b7a6
JunSuHeo <wmfkf95@gmail.com>
2021-06-20 16:41:56 +0900

    백준 2579

---
+++ Q2579/Main.java
@@ -0,0 +1,32 @@
+package Q2579;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] stairs = new int[n+1];
+		stairs[0] = 0;
+		
+		for(int i = 1; i <= n; i++) {
+			stairs[i] = Integer.parseInt(br.readLine());
+		}
+		int[] dp = new int[n+1];
+		dp[0] = 0;
+		dp[1] = stairs[1];
+		if(n >= 2) {
+			dp[2] = stairs[1] + stairs[2];
+		}
+		
+		for(int i = 3; i <= n; i++) {
+			dp[i] = Math.max(dp[i-3] + stairs[i-1], dp[i-2]) + stairs[i];
+		}
+		
+		System.out.println(dp[n]);
+	}
+
+}
